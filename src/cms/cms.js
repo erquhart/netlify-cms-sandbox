11ea28b4c40f0c51201619639877c53fb3db98e1
Ireneusz Luczak <luczak.irek@gmail.com>
2017-12-27 14:23:27 -0600

    adding menu boilerplate code

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,7 @@
 import CMS from "netlify-cms";
 
 import { SlidesControl, SlidesPreview } from "./Slides";
+import { MenuControl, MenuPreview } from "./Menu";
 
 CMS.registerWidget("slides", SlidesControl, SlidesPreview);
+CMS.registerWidget("menu", MenuControl, MenuPreview);
